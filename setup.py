ac4b1bdd5174eb595798769ddcabc17b7b8acda9
Tim Jensen <tim.l.jensen@gmail.com>
2021-07-25 20:43:07 -0500

    Adding build and install support

---
+++ setup.py
@@ -0,0 +1,44 @@
+from os import path
+import setuptools
+
+
+with open(path.join(path.abspath(path.dirname(__file__)), "README.md"), "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dayz-dev-tools",
+    version="1.0.0",
+    author="Tim Jensen",
+    author_email="tim.l.jensen@gmail.com",
+    description="Useful tools for DayZ mod developers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/tjensen/dayz-dev-tools",
+    project_urls={
+        "Source": "https://github.com/tjensen/dayz-dev-tools",
+        "Tracker": "https://github.com/tjensen/dayz-dev-tools/issues"
+    },
+    keywords=["dayz", "tools", "pbo"],
+    packages=["dayz"],
+    package_data={
+        "dayz": ["py.typed"]  # PEP 561
+    },
+    zip_safe=False,  # Enables mypy to find the installed package
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Games/Entertainment :: First Person Shooters",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Utilities",
+        "Typing :: Typed"
+    ],
+    python_requires=">=3.8",
+    entry_points={
+        "console_scripts": [
+            "unpbo=dayz.unpbo:main"
+        ]
+    })
