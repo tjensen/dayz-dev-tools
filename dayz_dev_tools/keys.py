8d762edccaf77edc28d2dcda466a537abf4b4231
Tim Jensen <tim.l.jensen@gmail.com>
2021-09-04 07:56:13 -0500

    Log warning when no *.bikey files are found for a mod so that users are notified of an issue that may prevent the mod from being loaded correctly.

--- dayz_dev_tools/keys.py
+++ dayz_dev_tools/keys.py
@@ -7,11 +7,11 @@ def copy_keys(source: str, destination: str) -> None:
     try:
         keys = [filename for filename in os.listdir(source) if filename.lower().endswith(".bikey")]
     except FileNotFoundError:
-        logging.debug(f"Unable to find bikey files to copy at {source}", exc_info=True)
-        return
+        logging.debug(f"Error when searching for *.bikey files to copy at {source}", exc_info=True)
+        keys = []
 
     if len(keys) == 0:
-        logging.debug(f"No bikey files found in {source}")
+        logging.warning(f"No *.bikey files found in {source}")
         return
 
     os.makedirs(destination, exist_ok=True)
