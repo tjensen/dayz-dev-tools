d2ca3a2c9e6a76becb49da18d525fc0c110190de
Tim Jensen <tim.l.jensen@gmail.com>
2021-09-04 14:07:05 -0500

    Removing view source links

--- docs/conf.py
+++ docs/conf.py
@@ -54,4 +54,6 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_copy_source = False
+
 autodoc_class_signature = 'separated'
