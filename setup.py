35b832f97c5bc202358c1a96b1d1b943aba1d06b
Tim Jensen <tim.l.jensen@gmail.com>
2023-12-04 18:18:01 -0600

    Bumping version for 1.6.0 release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(path.abspath(path.dirname(__file__)), "README.md"), "r", enc
 
 setuptools.setup(
     name="dayz-dev-tools",
-    version="1.6.dev0",
+    version="1.6.0",
     author="Tim Jensen",
     author_email="tim.l.jensen@gmail.com",
     description="Useful tools for DayZ mod developers.",
