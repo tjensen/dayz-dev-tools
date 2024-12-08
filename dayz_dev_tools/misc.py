98705dd532571b96a386d10a485b1ad931f4d257
Tim Jensen <tim.l.jensen@gmail.com>
2024-12-07 18:11:07 -0600

    Using `collections.abc` instead of `typing` for types that have been deprecated in the latter module

--- dayz_dev_tools/misc.py
+++ dayz_dev_tools/misc.py
@@ -1,10 +1,11 @@
+from collections import abc
 import contextlib
 import os
 import typing
 
 
 @contextlib.contextmanager
-def chdir(path: typing.Optional[str]) -> typing.Generator[None, None, None]:
+def chdir(path: typing.Optional[str]) -> abc.Generator[None, None, None]:
     if path is not None:
         original_cwd = os.getcwd()
         os.chdir(path)
