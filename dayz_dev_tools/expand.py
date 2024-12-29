6e6832b397f7ea7041a6010792fcef0591282984
Tim Jensen <tim.l.jensen@gmail.com>
2024-12-29 11:33:54 -0600

    Removing unnecessary expand Python module

--- dayz_dev_tools/expand.py
+++
@@ -1,7 +0,0 @@
-from dayz_dev_tools import pbo_file_reader
-import dayz_dev_tools_rust
-
-
-def expand(reader: pbo_file_reader.PBOFileReader, outsize: int) -> bytes:
-    inbuffer = reader.read(reader.size)
-    return dayz_dev_tools_rust.expand(inbuffer, outsize)
--- tests/test_expand.py
+++ tests/test_expand.py
@@ -1,15 +1,8 @@
-import io
 import unittest
 
-from dayz_dev_tools import expand
-from dayz_dev_tools import pbo_file_reader
+from dayz_dev_tools_rust import expand
 
 
 class TestExpand(unittest.TestCase):
     def test_expands_previously_compressed_data(self) -> None:
-        inbytes = b"\xffABCDEFGH\0\x07\x01"
-        infile = pbo_file_reader.PBOFileReader(io.BytesIO(inbytes), 0, len(inbytes))
-
-        outbytes = expand.expand(infile, 12)
-
-        assert outbytes == b"ABCDEFGHBCDE"
+        assert expand(b"\xffABCDEFGH\0\x07\x01", 12) == b"ABCDEFGHBCDE"
