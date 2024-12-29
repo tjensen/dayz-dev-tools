b9fc8346ba59be90f572c0b5e0aa9c27bd6306ee
Tim Jensen <tim.l.jensen@gmail.com>
2024-12-29 11:09:46 -0600

    Porting most of the expand unit tests to Rust

--- src/dayz_dev_tools_rust/src/lib.rs
+++ src/dayz_dev_tools_rust/src/lib.rs
@@ -57,13 +57,8 @@ impl<'a> InBuffer<'a> {
     }
 }
 
-#[pyfunction]
-fn expand<'p>(
-    py: Python<'p>,
-    input: &'p Bound<'p, PyBytes>,
-    capacity: usize,
-) -> PyResult<Bound<'p, PyBytes>> {
-    let mut raw = InBuffer::<'p>::new(input.as_bytes());
+fn expand_impl(inbytes: &[u8], capacity: usize) -> Vec<u8> {
+    let mut raw = InBuffer::new(inbytes);
     let mut output = Vec::with_capacity(capacity);
     'outer: while !raw.end() && output.len() < capacity {
         if let Ok(flagbyte) = raw.readbyte() {
@@ -105,10 +100,79 @@ fn expand<'p>(
             break 'outer;
         }
     }
+    output
+}
+
+#[pyfunction]
+fn expand<'p>(
+    py: Python<'p>,
+    input: &'p Bound<'p, PyBytes>,
+    capacity: usize,
+) -> PyResult<Bound<'p, PyBytes>> {
+    let output = expand_impl(input.as_bytes(), capacity);
+
     Ok(PyBytes::new(py, &output))
 }
 
 #[pymodule]
 fn dayz_dev_tools_rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(expand, m)?)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_expand_expands_trivially_compressed_file() {
+        let outbytes = expand_impl(b"\xffABCDEFGH\xffIJKLMNOP\xffQRSTUVWX", 24);
+
+        assert_eq!(outbytes, b"ABCDEFGHIJKLMNOPQRSTUVWX");
+    }
+
+    #[test]
+    fn test_expand_stops_extracting_bytes_when_end_of_content_is_reached_within_packet() {
+        assert_eq!(expand_impl(b"\xffABCDE", 5), b"ABCDE");
+    }
+
+    #[test]
+    fn test_expand_stops_extracting_bytes_when_end_of_output_buffer_is_reached() {
+        assert_eq!(
+            expand_impl(b"\xffABCDEFGH\xffIJKLMNOP\xffQRSTUVWX", 5),
+            b"ABCDE"
+        );
+    }
+
+    #[test]
+    fn test_expand_expands_previously_compressed_data() {
+        assert_eq!(expand_impl(b"\xffABCDEFGH\0\x07\x01", 12), b"ABCDEFGHBCDE");
+    }
+
+    #[test]
+    fn test_expand_does_not_overflow_output_size_when_expanding_previously_compressed_data() {
+        assert_eq!(expand_impl(b"\xffABCDEFGH\0\x07\x01", 10), b"ABCDEFGHBC");
+    }
+
+    #[test]
+    fn test_expand_inserts_spaces_when_compressed_data_references_negative_offset() {
+        assert_eq!(
+            expand_impl(b"\x0fABCD\x05\x0f", 22),
+            b"ABCD                  "
+        );
+    }
+
+    #[test]
+    fn test_expand_stops_inserting_spaces_when_end_of_output_buffer_is_reached() {
+        assert_eq!(expand_impl(b"\x0fABCD\x05\x0f", 10), b"ABCD      ");
+    }
+
+    #[test]
+    fn test_expand_repeats_previous_data_when_length_extends_beyond_end_of_data() {
+        assert_eq!(expand_impl(b"\x0fABCD\x02\x07", 14), b"ABCDCDCDCDCDCD");
+    }
+
+    #[test]
+    fn test_expand_does_not_repeat_to_insert_more_than_requested_number_of_characters() {
+        assert_eq!(expand_impl(b"\x0fABCD\x02\x08", 15), b"ABCDCDCDCDCDCDC");
+    }
 }
--- tests/test_expand.py
+++ tests/test_expand.py
@@ -6,30 +6,6 @@ from dayz_dev_tools import pbo_file_reader
 
 
 class TestExpand(unittest.TestCase):
-    def test_expands_trivially_compressed_file(self) -> None:
-        inbytes = b"\xffABCDEFGH\xffIJKLMNOP\xffQRSTUVWX"
-        infile = pbo_file_reader.PBOFileReader(io.BytesIO(inbytes), 0, len(inbytes))
-
-        outbytes = expand.expand(infile, 24)
-
-        assert outbytes == b"ABCDEFGHIJKLMNOPQRSTUVWX"
-
-    def test_stops_extracting_bytes_when_end_of_content_is_reached_within_packet(self) -> None:
-        inbytes = b"\xffABCDE"
-        infile = pbo_file_reader.PBOFileReader(io.BytesIO(inbytes), 0, len(inbytes))
-
-        outbytes = expand.expand(infile, 5)
-
-        assert outbytes == b"ABCDE"
-
-    def test_stops_extracting_bytes_when_end_of_output_buffer_is_reached(self) -> None:
-        inbytes = b"\xffABCDEFGH\xffIJKLMNOP\xffQRSTUVWX"
-        infile = pbo_file_reader.PBOFileReader(io.BytesIO(inbytes), 0, len(inbytes))
-
-        outbytes = expand.expand(infile, 5)
-
-        assert outbytes == b"ABCDE"
-
     def test_expands_previously_compressed_data(self) -> None:
         inbytes = b"\xffABCDEFGH\0\x07\x01"
         infile = pbo_file_reader.PBOFileReader(io.BytesIO(inbytes), 0, len(inbytes))
@@ -37,43 +13,3 @@ class TestExpand(unittest.TestCase):
         outbytes = expand.expand(infile, 12)
 
         assert outbytes == b"ABCDEFGHBCDE"
-
-    def test_does_not_overflow_output_size_when_expanding_previously_compressed_data(self) -> None:
-        inbytes = b"\xffABCDEFGH\0\x07\x01"
-        infile = pbo_file_reader.PBOFileReader(io.BytesIO(inbytes), 0, len(inbytes))
-
-        outbytes = expand.expand(infile, 10)
-
-        assert outbytes == b"ABCDEFGHBC"
-
-    def test_inserts_spaces_when_compressed_data_references_negative_offset(self) -> None:
-        inbytes = b"\x0fABCD\x05\x0f"
-        infile = pbo_file_reader.PBOFileReader(io.BytesIO(inbytes), 0, len(inbytes))
-
-        outbytes = expand.expand(infile, 22)
-
-        assert outbytes == b"ABCD                  "
-
-    def test_stops_inserting_spaces_when_end_of_output_buffer_is_reached(self) -> None:
-        inbytes = b"\x0fABCD\x05\x0f"
-        infile = pbo_file_reader.PBOFileReader(io.BytesIO(inbytes), 0, len(inbytes))
-
-        outbytes = expand.expand(infile, 10)
-
-        assert outbytes == b"ABCD      "
-
-    def test_repeats_previous_data_when_length_extends_beyond_end_of_data(self) -> None:
-        inbytes = b"\x0fABCD\x02\x07"
-        infile = pbo_file_reader.PBOFileReader(io.BytesIO(inbytes), 0, len(inbytes))
-
-        outbytes = expand.expand(infile, 14)
-
-        assert outbytes == b"ABCDCDCDCDCDCD"
-
-    def test_does_not_repeat_to_insert_more_than_requested_number_of_characters(self) -> None:
-        inbytes = b"\x0fABCD\x02\x08"
-        infile = pbo_file_reader.PBOFileReader(io.BytesIO(inbytes), 0, len(inbytes))
-
-        outbytes = expand.expand(infile, 15)
-
-        assert outbytes == b"ABCDCDCDCDCDCDC"
